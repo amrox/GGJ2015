a01799ebfb9d240e5e3e1b81eb0f696d8580af3e
Andy Mroczkowski <andy@mrox.net>
2015-01-24 12:23:41 -0500

    more stuff

--- day2/hacker.py
+++ day2/hacker.py
@@ -2,36 +2,54 @@
 
 import socket
 import sys
+from threading import Thread, Event
 
-#def client(string):
-#    HOST, PORT = 'localhost', 2000
-#    # SOCK_STREAM == a TCP socket
-#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    #sock.setblocking(0)  # optional non-blocking
-#    sock.connect((HOST, PORT))
-#    sock.send(string)
-#    reply = sock.recv(16384)  # limit reply to 16K
-#    sock.close()
-#    return reply
-#
-#assert client('2+2') == '4'
-#
 
-def connect(host, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #sock.setblocking(0)  # optional non-blocking
-    sock.connect((host, port))
+class ReceiveThread(Thread):
+    def __init__(self, event, sock):
+        Thread.__init__(self)
+        self.stopped = event
+        self.sock = sock
 
-    while True:
+    def run(self):
+        while not self.stopped.wait(0.5):
+            try:
+                data = self.sock.recv(16384)  # limit reply to 16K
+                #print reply
+                if data.strip() == "LOST":
+                    print "YOU LOSE"
+                    break
+
+            except socket.error as ex:
+                if str(ex) == "[Errno 35] Resource temporarily unavailable":
+                    continue
+                raise
+
+
+class Client(object):
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+        self.receiveThread = None
+        self.stopFlag = Event()
+
+    def connect(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        sock.setblocking(0)  # optional non-blocking
     
-        reply = sock.recv(16384)  # limit reply to 16K
-    
-    
-    #sock.send(string)
+        self.receiveThread = ReceiveThread(self.stopFlag, sock)
+        self.receiveThread.start()
 
+    def stop(self):
+        self.stopFlag.set()
+    
 
 def usage():
     return "hacker.py [host] [port]"
+
 
 def main():
 
@@ -42,10 +60,17 @@ def main():
     host = sys.argv[1]
     port = int(sys.argv[2])
 
-    connect(host, port)
+    c = Client(host, port)
+    c.connect()
+
+    while c.receiveThread.isAlive():
+        try:
+            cmd = raw_input('> ')
+            print "%s" % (cmd)
+        except KeyboardInterrupt:
+            c.stop()
+            sys.exit(0)
 
 
-# Standard boilerplate to call the main() function to begin
-# the program.
 if __name__ == '__main__':
     main()
